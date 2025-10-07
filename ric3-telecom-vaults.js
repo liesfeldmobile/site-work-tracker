f9630a25e9d41eebe4d553b3690a8e49f06688f6
liesfeldmobile <jmelton@liesfeld.com>
2025-10-07 04:12:51 -0700

    Add files via upload

--- ric3-telecom-vaults.js
+++ ric3-telecom-vaults.js
@@ -1,5 +1,372 @@
-
 window.RIC3_TELECOM_VAULTS = [
-  { campus: "RIC3", building: "DC2", vaultId: "TMH-41", category: "Telecom", status: "Open", notes: "" },
-  { campus: "RIC3", building: "DC2", vaultId: "TMH-42", category: "Telecom", status: "Closed", notes: "" }
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "BEV 02"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "BEV 03"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "BEV 04"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "BEV 08"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "CAV 01"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "CAV 02"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 01"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 02"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 03"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 04"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "BEV 01"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 06"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 07"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 23"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 24"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 25"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 26"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 27"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 28"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 29"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 30"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 31"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 38"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 39"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 46"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 47"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 48"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 49"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 50"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC1/Sub",
+    "vaultId": "TMH 51"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC2",
+    "vaultId": "BEV 05"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC2",
+    "vaultId": "BEV 06"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC2",
+    "vaultId": "BEV 07"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC2",
+    "vaultId": "BEV 11"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC2",
+    "vaultId": "TMH 08"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC2",
+    "vaultId": "TMH 09"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC2",
+    "vaultId": "TMH 45"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC2",
+    "vaultId": "TMH 21"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC2",
+    "vaultId": "TMH 22"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC2",
+    "vaultId": "TMH 40"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC2",
+    "vaultId": "TMH 44"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC2",
+    "vaultId": "TMH 52"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC2",
+    "vaultId": "TMH 53"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC3",
+    "vaultId": "BEV 09"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC3",
+    "vaultId": "BEV 10"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC3",
+    "vaultId": "BEV 12"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC3",
+    "vaultId": "TMH 10"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC3",
+    "vaultId": "TMH 11"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC3",
+    "vaultId": "TMH 12"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC3",
+    "vaultId": "TMH 13"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC3",
+    "vaultId": "TMH 14"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC3",
+    "vaultId": "TMH 41"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC4",
+    "vaultId": "BEV 14"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC4",
+    "vaultId": "BEV 16"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC4",
+    "vaultId": "BEV 13"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC4",
+    "vaultId": "BEV 15"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC4",
+    "vaultId": "TMH 32"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC4",
+    "vaultId": "TMH 33"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC4",
+    "vaultId": "TMH 34"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC4",
+    "vaultId": "TMH 35"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC4",
+    "vaultId": "TMH 36"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC4",
+    "vaultId": "TMH 37"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC5",
+    "vaultId": "TMH 20"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC5",
+    "vaultId": "BEV 18"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC5",
+    "vaultId": "BEV 20"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC5",
+    "vaultId": "BEV 17"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC5",
+    "vaultId": "BEV 19"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC5",
+    "vaultId": "TMH 15"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC5",
+    "vaultId": "TMH 16"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC5",
+    "vaultId": "TMH 17"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC5",
+    "vaultId": "TMH 18"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC5",
+    "vaultId": "TMH 19"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC5",
+    "vaultId": "TMH 43"
+  },
+  {
+    "campus": "RIC3",
+    "building": "DC6",
+    "vaultId": "TMH 42"
+  }
 ];
